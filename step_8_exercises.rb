d99d8438d7edb4d78cc6876ffdd666ebb7b1b1f3
Aimee <aimeehorwood@hotmail.com>
2022-11-03 11:05:04 +0000

    Adds 8.5

--- step_8_exercises.rb
+++ step_8_exercises.rb
@@ -1,5 +1,5 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names and details of students"
   puts "To finish, just hit return twice"
 
   students = []
@@ -7,7 +7,14 @@ def input_students
   name = gets.chomp
 
   while !name.empty?
-    students << { name: name, cohort: :november }
+    puts "What are their hobbies?"
+    hobbies = gets.chomp
+    puts "What is their birth place?"
+    birth_place = gets.chomp
+    puts "What is their height in cm?"
+    height = gets.chomp
+
+    students << { name: name, hobbies: hobbies, birth_place: birth_place, height: height, cohort: :november }
     puts "Now we have #{students.count} students"
 
     name = gets.chomp
@@ -21,11 +28,10 @@ def print_header
   puts "-------------"
 end
 
-def print_loop(students)
-  i = 0
-  while i < students.length
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
-    i += 1
+def print(students)
+  students.each.with_index do |student, index|
+    puts " #{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "They enjoy #{student[:hobbies]},they were born in #{student[:birth_place]} and they are #{student[:height]}cm tall."
   end
 end
 
@@ -36,7 +42,7 @@ end
 students = input_students
 
 print_header
-print_loop(students)
+print(students)
 print_footer(students)
 
 # 8.1 How can you modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter"?
@@ -72,3 +78,6 @@ print_footer(students)
 #    i += 1
 #  end
 # end
+
+# 8.5 Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc.
+# As above and each_with_index amended to puts new information about students
